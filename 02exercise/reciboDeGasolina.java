aa4d328ed020fd2ce9c6b2fc0c13a0079407b76a
enigmak9 <enigmak9@protonmail.com>
2024-06-15 21:18:35 -0600

    second drill added

---
+++ 02exercise/reciboDeGasolina.java
@@ -0,0 +1,16 @@
+public class gasolineReceipt {
+    public static void main( String[] args ) {
+        System.out.println("+------------------------+");
+        System.out.println("|                        |");
+        System.out.println("| Gasolinera del Charles |");
+        System.out.println("|                        |");
+        System.out.println("| 2024-06-15  04:38PM    |");
+        System.out.println("|                        |");
+        System.out.println("| Litros:       41.14    |");
+        System.out.println("| Precio: $ 23.52        |");
+        System.out.println("|                        |");
+        System.out.println("| Total:       $ 967.61  |");
+        System.out.println("|                        |");
+        System.out.println("+------------------------+");
+    }
+}
