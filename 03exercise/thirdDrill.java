4223310c4b30ee826c4a99f4fcf9938a85b52483
enigmak9 <enigmak9@protonmail.com>
2024-06-15 21:30:01 -0600

    third exercise and third Drill added

---
+++ 03exercise/thirdDrill.java
@@ -0,0 +1,13 @@
+public class thirdDrill {
+    public static void main(String[] args){
+        System.out.println("I");
+        System.out.println(" am");
+        System.out.println(" learning");
+        System.out.println(" Java");
+        System.out.println();
+        System.out.println(" the");
+        System.out.println(" Hard");
+        System.out.println("Way");
+        System.out.println("I" + " am" + " learning" + " Java" + "the" + " hard Way! ");
+    }
+}
