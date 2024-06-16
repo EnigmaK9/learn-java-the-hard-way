65cc1581a8f57bf92899749a4e06c465dc65f18a
enigmak9 <enigmak9@protonmail.com>
2024-06-15 21:02:47 -0600

    first study drill added

---
+++ 01exercise/firstStudyDrill.java
@@ -0,0 +1,6 @@
+public class firstStudyDrill {
+    public static void main(String[] args) {
+        System.out.println("Today's date is Saturday, 15 june 2024.");
+        System.out.println("I, EnigmaK9, am determined to learn how to code.");
+        }
+    }
