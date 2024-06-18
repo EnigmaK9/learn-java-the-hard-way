2d86c4286d508a1b5c1172d335283b4d72ca4b3a
enigmak9 <enigmak9@protonmail.com>
2024-06-17 22:14:09 -0600

    added fifth study drill and fifth exercise

---
+++ 05exercise/StudyDrill.java
@@ -0,0 +1,29 @@
+public class StudyDrill {
+    public static void main( String[] args) {
+        int x, y, age, powerlevel;
+        double seconds, e, checking, pi;
+        String firstName, lastName, title;
+
+        x = 10;
+        y = 400;
+        age = 39;
+
+        seconds = 4.71;
+        e = 2.71828182845904523536;
+        checking = 1.89;
+        pi = 3.14159;
+
+        firstName = "Enigma ";
+        lastName = "K9" ;
+        title = "Engineer. ";
+
+
+        System.out.println( "The variable x contains " + x );
+        System.out.println( "The value " + y + " is stored in the variable y. " );
+        System.out.println( "The experiment took " + seconds + " seconds." );
+        System.out.println( "A favorite irrational number is Euler's number: " + e );
+        System.out.println( "A favorite irrational number is pi number: " + pi );
+        System.out.println( "Hopefully you have more than $" + checking + "!" );
+        System.out.println( "My name's " + title + " " + firstName + lastName );
+    }
+}
